320d092eacf284c41a98766cbdc895f092f4dd6b
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-07-13 11:35:28 +0200

    Added a line break setting/flag to DebugPrinter.

--- src/main/java/nwawsoft/util/DebugPrinter.java
+++ src/main/java/nwawsoft/util/DebugPrinter.java
@@ -9,8 +9,8 @@ public class DebugPrinter {
     /**
      * Prints which method called this one and prints the passed String if the DEBUG flag is set.
      *
-     * @param classObject     the class calling this method. Usually "this".
-     * @param printableString the String to print.
+     * @param classObject       the class calling this method. Usually "this".
+     * @param printableString   the String to print.
      */
     private static void debugPrint(final Object classObject, final String printableString) {
         if (DebugPrinter.DEBUG) {
@@ -33,9 +33,50 @@ public class DebugPrinter {
     }
 
     /**
+     * Prints which method called this one and prints the passed String if the DEBUG flag is set.
+     *
+     * @param classObject       the class calling this method. Usually "this".
+     * @param printableString   the String to print.
+     * @param lineMode          0 or 1. 0 for line break after print, 1 for no line break.
+     */
+    private static void debugPrint(final Object classObject, final String printableString, int lineMode) {
+        if (DebugPrinter.DEBUG) {
+            String method = classObject.getClass().getDeclaredMethods()[0].toString();
+            String methodPath = "";
+            String methodName = Thread.currentThread().getStackTrace()[3].getMethodName();
+            if (method.contains("(")) {
+                methodPath = method.substring(0, method.indexOf('('));
+                if (methodPath.contains(".")) {
+                    methodPath = methodPath.substring(0, methodPath.lastIndexOf('.'));
+                }
+            }
+            if (methodName.equals("<init>")) {
+                if (lineMode == 0) {
+                    System.out.println("DebugPrinter for " + methodPath + " (constructor):");
+                } else if (lineMode == 1) {
+                    System.out.print("DebugPrinter for " + methodPath + " (constructor): ");
+                }
+            } else {
+                if (lineMode == 0) {
+                    System.out.println("DebugPrinter for " + methodPath + "." + methodName + ":");
+                }
+                else if (lineMode == 1) {
+                    System.out.print("DebugPrinter for " + methodPath + "." + methodName + ":" );
+                }
+            }
+            if (lineMode == 0) {
+                System.out.println(printableString);
+            }
+            else if (lineMode == 1) {
+                System.out.print(printableString);
+            }
+        }
+    }
+
+    /**
      * Prints the passed String if the DEBUG flag is set.
      *
-     * @param printableString the String to print.
+     * @param printableString   the String to print.
      */
     private static void debugPrint(final String printableString) {
         if (DebugPrinter.DEBUG) {
@@ -44,22 +85,59 @@ public class DebugPrinter {
     }
 
     /**
+     * Prints the passed String if the DEBUG flag is set.
+     *
+     * @param printableString   the String to print.
+     * @param lineMode          0 or 1. 0 for line break after print, 1 for no line break.
+     */
+    private static void debugPrint(final String printableString, int lineMode) {
+        if (DebugPrinter.DEBUG) {
+            if (lineMode == 0) {
+                System.out.println(printableString);
+            } else if (lineMode == 1) {
+                System.out.print(printableString);
+            }
+        }
+    }
+
+    /**
      * Wrapper for debugPrint(Class, String).
      *
-     * @param classObject     the class calling this method. Usually "this"
-     * @param printableString the String to print.
+     * @param classObject       the class calling this method. Usually "this"
+     * @param printableString   the String to print.
      */
     public static void dp(final Object classObject, final String printableString) {
         debugPrint(classObject, printableString);
     }
 
     /**
+     * Wrapper for debugPrint(Class, String, int).
+     *
+     * @param classObject       the class calling this method. Usually "this"
+     * @param printableString   the String to print.
+     * @param lineMode          0 or 1. 0 for line break after print, 1 for no line break.
+     */
+    public static void dp(final Object classObject, final String printableString, int lineMode) {
+        debugPrint(classObject, printableString, 0);
+    }
+
+    /**
      * Wrapper for debugPrint(String).
      *
-     * @param printableString the String to print.
+     * @param printableString   the String to print.
      */
     public static void dp(final String printableString) {
         debugPrint(printableString);
+    }
+
+    /**
+     * Wrapper for debugPrint(String, int).
+     *
+     * @param printableString   the String to print.
+     * @param lineMode          0 or 1. 0 for line break after print, 1 for no line break.
+     */
+    public static void dp(final String printableString, int lineMode) {
+        debugPrint(printableString, lineMode);
     }
 
     /**
@@ -67,8 +145,8 @@ public class DebugPrinter {
      * If it is not set only the passed String will be shown.
      * If it is set the calling method's path and name will also be shown.
      *
-     * @param classObject the class calling this method. Usually "this".
-     * @param printableString the String to print.
+     * @param classObject       the class calling this method. Usually "this".
+     * @param printableString   the String to print.
      */
     public static void p (final Object classObject, final String printableString) {
         if (DebugPrinter.DEBUG) {
