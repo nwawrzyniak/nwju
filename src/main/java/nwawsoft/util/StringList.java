96cab9f15d40456d6f598a92aee1502609b65cfb
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-07-20 04:25:35 +0200

    StringList overhaul. Added documentation. Improved concat(List). Added printHere and printHere(int outputAmount).

--- src/main/java/nwawsoft/util/StringList.java
+++ src/main/java/nwawsoft/util/StringList.java
@@ -1,12 +1,24 @@
 package nwawsoft.util;
 
 /**
- * Works like list but ignores objects except Strings and lists that are not purely made out of
- * Strings.
- * <p>
- * Created by Ernst on 11.07.2017.
+ * Works like List but ignores objects except Strings and List objects that are not purely made out of Strings.
+ * Whenever the documentation works with the words Object and object make sure to check the capitalization to
+ * differentiate between the class that every class extends and any regular object.
  */
 public class StringList extends List {
+
+    /**
+     * Creates a new StringList object.
+     */
+    public StringList() {
+        super();
+    }
+
+    /**
+     * Sets the Object where the access pointer is, but only if it is a String.
+     *
+     * @param object an Object to set to the current position of the StringList.
+     */
     @Override
     public void setObject(Object object) {
         if (object instanceof String) {
@@ -14,6 +26,11 @@ public class StringList extends List {
         }
     }
 
+    /**
+     * Appends the Object to the end of the StringList, but only if it is a String.
+     *
+     * @param object an Object to append to the StringList.
+     */
     @Override
     public void append(Object object) {
         if (object instanceof String) {
@@ -21,6 +38,11 @@ public class StringList extends List {
         }
     }
 
+    /**
+     * Inserts the Object in front of where the access pointer is, but only if it is a String.
+     *
+     * @param object an Object to insert into the StringList.
+     */
     @Override
     public void insert(Object object) {
         if (object instanceof String) {
@@ -28,22 +50,79 @@ public class StringList extends List {
         }
     }
 
+    /**
+     * Concatenates the specified List to the end of the List making this call. Ignores all non-String values.
+     *
+     * @param pList a List whose contents shall be appended to the StringList.
+     */
     @Override
-    public void concat(List pList) {
+    public void concat(final List pList) {
         if (pList instanceof StringList) {
             super.concat(pList);
+        } else {
+            pList.toFirst();
+            while (pList.hasAccess()) {
+                if (pList.getObject() instanceof String) {
+                    this.append(pList.getObject());
+                }
+                next();
+            }
         }
     }
 
     /**
-     * Prints the whole StringList one String per line and moves the access pointer to the first
-     * object.
+     * Prints the whole StringList one String per line and moves the access pointer to the first entry.
      */
     public void print() {
         super.toFirst();
         while (super.hasAccess()) {
-            System.out.println((String) super.getObject());
+            System.out.println((String)super.getObject());
             super.next();
+        }
+        super.toFirst();
+    }
+
+    /**
+     * Prints the whole StringList with a maximum of outputAmount Strings one String per line and moves the access
+     * pointer to the first entry.
+     *
+     * @param outputAmount the maximum number of entries to print, starting at the beginning.
+     */
+    public void print(final int outputAmount) {
+        super.toFirst();
+        int count = 0;
+        while (super.hasAccess() && count <= outputAmount) {
+            count++;
+            System.out.println((String)super.getObject());
+            super.next();
+        }
+        super.toFirst();
+    }
+
+    /**
+     * Prints the rest of the StringList, starting from where the access pointer was, one String per line and moves the
+     * access pointer to the first entry.
+     */
+    public void printHere() {
+        while (super.hasAccess()) {
+            System.out.println((String)super.getObject());
+            super.next();
+        }
+        super.toFirst();
+    }
+
+    /**
+     * Prints the rest of the StringList with a maximum of outputAmount Strings, starting from where the access pointer
+     * was, one String per line and moves the access pointer to the first entry.
+     *
+     * @param outputAmount the maximum number of entries to print, starting where the access pointer is.
+     */
+    public void printHere(final int outputAmount) {
+        int count = 0;
+        while (super.hasAccess() && count <= outputAmount) {
+            System.out.println((String)super.getObject());
+            super.next();
+            count++;
         }
         super.toFirst();
     }
