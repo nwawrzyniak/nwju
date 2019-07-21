6abd7ce4c6075294c22cee89fe0c4463ddb4564d
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-07-21 07:54:46 +0200

    Overhauled class ExtensionTools.

--- src/main/java/nwawsoft/util/ExtensionTools.java
+++ src/main/java/nwawsoft/util/ExtensionTools.java
@@ -2,25 +2,29 @@ package nwawsoft.util;
 
 import java.io.File;
 
+/**
+ * Provides functions to work with file extensions.
+ */
 public class ExtensionTools {
-    private String jpeg = "jpeg";
-    private String jpg = "jpg";
-    private String gif = "gif";
-    private String tiff = "tiff";
-    private String tif = "tif";
-    private String png = "png";
 
-    public ExtensionTools() {
-
-    }
-
+    /**
+     * Returns the file extension of a specified File as a String (without the leading '.').
+     * If there is no file extension an empty String is returned.
+     *
+     * @param f a File object.
+     * @return the file extension of f as a String.
+     */
     public static String getExtension(File f) {
         String ext = null;
         String s = f.getName();
-        int i = s.lastIndexOf('.');
-        if (i > 0 && i < s.length() - 1) {
-            ext = s.substring(i + 1).toLowerCase();
+        if (s.contains(".")) {
+            int i = s.lastIndexOf('.');
+            if (i > 0 && i < s.length() - 1) {
+                ext = s.substring(i + 1).toLowerCase();
+            }
+            return ext;
         }
-        return ext;
+        DebugPrinter.dp("No file extension found. Returning empty String.");
+        return "";
     }
 }
