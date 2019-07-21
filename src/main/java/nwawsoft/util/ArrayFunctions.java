b2e5d239c403b7a6612b39c62a8f5ff8c1ae3c4a
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-07-21 08:36:22 +0200

    Overhauled class ArrayFunctions.

--- src/main/java/nwawsoft/util/ArrayFunctions.java
+++ src/main/java/nwawsoft/util/ArrayFunctions.java
@@ -8,98 +8,108 @@ package nwawsoft.util;
  */
 public class ArrayFunctions
 {
-  public ArrayFunctions()
-  {
-    
-  }
-  
   /**
-   * Returns an Object-Array with the size of the old Array + an Integer.
-   * It contains as much of the old Array's content as possible.
+   * Returns an array with size oldArray.length+additions.
+   * It contains as much of the old Array's content as possible, starting at index 0 and filling the additional spaces
+   * with null.
+   *
+   * @param oldArray an array of any type.
+   * @param additions how many spaces more the new array shall have.
+   * @return an array with size oldArray.length+additions which contains as much content of oldArray as possible.
    */
-  public Object[] expandArray(Object[] oldArray, int additions)
+  public static Object[] expandArray(final Object[] oldArray, final int additions)
   {
     int oldSize = oldArray.length;
     int newSize = oldSize + additions;
-    Object[] biggerArray = new Object[newSize];
-    for (int i = 0; i < oldSize; i++)
-    {
-      biggerArray[i] = oldArray[i];
+    Object[] newArray = new Object[newSize];
+    System.arraycopy(oldArray, 0, newArray, 0, oldSize);
+    for (int i = oldSize; i < newSize; i++) {
+      try {
+        newArray[i] = null;
+      } catch (Exception e) {
+        DebugPrinter.dp("Tried to write null to a data type that does not accept null. Skipping.");
+      }
     }
-    return biggerArray;
+    return newArray;
   }
-  
+
   /**
-   * Returns an Object-Array with the size of the old Array - an Integer.
+   * Returns an array with size oldArray.length-subtractions.
    * It contains as much of the old Array's content as possible.
+   *
+   * @param oldArray an array of any type.
+   * @param subtractions how many spaces less the new array shall have.
+   * @return an array with size oldArray.length-subtractions which contains as much content of oldArray as possible.
    */
-  public Object[] reduceArray(Object[] oldArray, int subtractions)
+  public static Object[] reduceArray(final Object[] oldArray, final int subtractions)
   {
     int oldSize = oldArray.length;
     int newSize = oldSize-subtractions;
-    Object[] smallerArray = new Object[newSize];
-    for (int i = 0; i < newSize; i++)
-    {
-      smallerArray[i] = oldArray[i];
-    }
-    return smallerArray;
+    Object[] newArray = new Object[newSize];
+    System.arraycopy(oldArray, 0, newArray, 0, newSize);
+    return newArray;
   }
-  
+
   /**
    * Returns an Object-Array with the size of the old Array - 1.
    * It contains all information of the old Array's except the first content gets deleted.
    * All contents move one position more to the beginning of the Array.
+   *
+   * @param oldArray an array of any type.
+   * @return a new array of size oldArray-1 with the contents of oldArray, except for entry oldArray[0].
    */
-  public Object[] deleteFirst(Object[] oldArray)
+  public static Object[] deleteFirst(final Object[] oldArray)
   {
     int oldSize = oldArray.length;
-    Object[] smallerArray = new Object[oldSize-1];
-    for (int i = 0; i < oldSize-1; i++)
-    {
-      smallerArray[i] = oldArray[i+1];
-    }
-    return smallerArray;
+    Object[] newArray = new Object[oldSize-1];
+    System.arraycopy(oldArray, 1, newArray, 0, oldSize - 1);
+    return newArray;
   }
-  
+
   /**
    * Returns an Object-Array with the specified size.
    * It contains as much of the old Array's content as possible.
+   *
+   * @param oldArray an array of any type.
+   * @param newSize the size of the new array.
+   * @return an array with size newSize and as much content of oldArray as possible.
    */
-  public Object[] changeArraySize(Object[] oldArray, int newSize)
+  public static Object[] changeArraySize(final Object[] oldArray, final int newSize)
   {
     int oldSize = oldArray.length;
     Object[] newArray = new Object[newSize];
     if (newSize > oldSize)
     {
-      for (int i=0; i < oldSize; i++)
-      {
-        newArray[i] = oldArray[i];
+      System.arraycopy(oldArray, 0, newArray, 0, oldSize);
+      for (int i = oldSize; i < newSize; i++) {
+        try {
+          newArray[i] = null;
+        } catch (Exception e) {
+          DebugPrinter.dp("Tried to write null to a data type that does not accept null. Skipping.");
+        }
       }
-    }
-    if (newSize < oldSize)
-    {
-      for (int i = 0; i < newSize; i++)
-      {
-        newArray[i] = oldArray[i];
-      }
-    }
-    if (newSize == oldSize)
-    {
-      for (int i = 0; i < newSize; i++)
-      {
-        newArray[i] = oldArray[i];
-      }
+    } else {
+      System.arraycopy(oldArray, 0, newArray, 0, newSize);
     }
     return newArray;
   }
-  
+
   /**
    * Returns an empty Object-Array with the size of the old Array.
+   *
+   * @param oldArray an array of any type.
+   * @return an empty array with the same size as oldArray.
    */
-  public Object[] clear(Object[] oldArray)
+  public static Object[] clear(final Object[] oldArray)
   {
-    int oldSize = oldArray.length;
-    Object[] smallerArray = new Object[oldSize];
-    return smallerArray;
+    Object[] newArray = new Object[oldArray.length];
+    for (int i = 0; i < newArray.length; i++) {
+      try {
+        newArray[i] = null;
+      } catch (Exception e) {
+        DebugPrinter.dp("Tried to write null to a data type that does not accept null. Skipping.");
+      }
+    }
+    return newArray;
   }
 }
