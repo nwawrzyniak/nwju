d0080bdda1ab5e0217c4f107fce2571c9b99f04d
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-07-29 21:00:19 +0200

    Renamed ExtensionTools to ExtensionFunctions to be in line with the other Functions classes. Reworked documentation.

--- src/main/java/nwawsoft/util/ExtensionFunctions.java
+++ src/main/java/nwawsoft/util/ExtensionFunctions.java
@@ -5,13 +5,13 @@ import java.io.File;
 /**
  * Provides functions to work with file extensions.
  */
-public class ExtensionTools {
+public class ExtensionFunctions {
     /**
      * Returns the file extension of a specified File as a String (without the leading '.').
      * If there is no file extension an empty String is returned.
      *
      * @param f a File object.
-     * @return the file extension of f as a String.
+     * @return the file extension of f as a String without the leading dot or an empty String if there is none.
      */
     public static String getExtension(final File f) {
         String ext = null;
