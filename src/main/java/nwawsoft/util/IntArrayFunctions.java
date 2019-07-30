74eb2d3e794553774e5d8652ad37851f8c6200ee
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-07-30 18:12:07 +0200

    Overhauled class IntArrayFuctions.java.

--- src/main/java/nwawsoft/util/IntArrayFunctions.java
+++ src/main/java/nwawsoft/util/IntArrayFunctions.java
@@ -1,5 +1,8 @@
 package nwawsoft.util;
 
+/**
+ * Provides functions to work with arrays of type int.
+ */
 public class IntArrayFunctions {
     public static final int MODE_LINE_BREAK = 0;
     public static final int MODE_COMMA = 1;
@@ -8,6 +11,7 @@ public class IntArrayFunctions {
      * Prints an array of Integers, one value per line.
      *
      * @param ints any int array
+     * @param mode 0 if values should be printed one line per number, 1 if you want them comma separated.
      */
     public static void print(int[] ints, int mode) {
         boolean firstRun = true;
@@ -25,33 +29,45 @@ public class IntArrayFunctions {
                 }
             }
         } else {
-            throw new IllegalArgumentException("ints shall not be null");
+            throw new IllegalArgumentException("int[] ints must not be null.");
         }
     }
 
+    /**
+     * Returns whether two int arrays have no shared value.
+     *
+     * @param a an int array.
+     * @param b another int array.
+     * @return true if no entry was found in both arrays, else false.
+     */
     public static boolean sharesNoEntry(int[] a, int[] b) {
-        boolean foundMatch = false;
-        for (int someA : a) {
-            for (int someB : b) {
-                if (someA == someB) {
-                    foundMatch = true;
+        for (int entryA : a) {
+            for (int entryB : b) {
+                if (entryA == entryB) {
+                    return false;
                 }
             }
         }
-        return (!foundMatch);
+        return true;
     }
 
+    /**
+     * Returns whether two int arrays have exactly the same values in the same order.
+     *
+     * @param a an int array.
+     * @param b another int array.
+     * @return true if a and b have the same contents in the same order, else false.
+     */
     public static boolean isEqual(int[] a, int[] b) {
-        boolean noError = true;
         if (a.length == b.length) {
             for (int i = 0; i < a.length; i++) {
                 if (a[i] != b[i]) {
-                    noError = false;
+                    return false;
                 }
             }
         } else {
-            noError = false;
+            return false;
         }
-        return noError;
+        return true;
     }
 }
