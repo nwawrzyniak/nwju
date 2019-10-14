3dd972af181e2e5f44a479e15097a56ecbefb857
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-10-14 19:03:41 +0200

    Added class MutatedVowels to handle German characters. These characters work in UTF-8, ANSI, Unicode and ISO-8859-1 and while embedded in HTML.

---
+++ src/main/java/nwawsoft/util/MutatedVowels.java
@@ -0,0 +1,15 @@
+package nwawsoft.util;
+
+public class MutatedVowels {
+    public static String aE = "\u00c4";
+    public static String ae = "\u00e4";
+    public static String oE = "\u00d6";
+    public static String oe = "\u00f6";
+    public static String uE = "\u00dc";
+    public static String ue = "\u00fc";
+    public static String ss = "\u00df";
+
+    public static void main(String[] args) {
+        System.out.println(aE + ae + oE + oe + uE + ue + ss);
+    }
+}
