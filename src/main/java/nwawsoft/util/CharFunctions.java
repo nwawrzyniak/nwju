95cbe3870edc8668b240aeb4c9e3ae3b51bf63b7
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-10-15 20:43:09 +0200

    Added more CharFunctions.

--- src/main/java/nwawsoft/util/CharFunctions.java
+++ src/main/java/nwawsoft/util/CharFunctions.java
@@ -12,6 +12,51 @@ public class CharFunctions {
    */
   public static boolean isNumeric(final char c)
   {
-    return ((int) c >= 48 && (int) c <= 57) || (int) c == 44 || (int) c == 46;
+    return (c >= '0' && c <= '9') || c == '.' || c == ',';
+  }
+
+  /**
+   * Returns whether a specified character is one of the 26 standard lower case 
+   * letters from 'a' to 'z'.
+   * 
+   * @param c a char to check.
+   * @return true if c is a lower case letter. Else false.
+   */
+  public static boolean isLowerCaseLetter(final char c) {
+    return c >= 'a' && c <= 'z';
+  }
+
+  /**
+   * Returns whether a specified character is one of the 26 standard upper case 
+   * letters from 'A' to 'Z'.
+   * 
+   * @param c a char to check.
+   * @return true if c is a upper case letter. Else false.
+   */
+  public static boolean isUpperCaseLetter(final char c) {
+    return c >= 'A' && c <= 'Z';
+  }
+
+  /**
+   * Returns whether a specified character is a digit between 0 and 9.
+   * 
+   * @param c a char to check.
+   * @return true if c is a digit. Else false.
+   */
+  public static boolean isDigit(final char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  /**
+   * Returns true, if the specified character is neither a lower case letter, an 
+   * upper case letter or a digit. For more info
+   * @see isLowerCaseLetter @see isUpperCaseLetter @see isDigit
+   * 
+   * @param c a char to check.
+   * @return true if c is neither an upper case or lower case letter or a digit.
+   * 			False if it is.
+   */
+  public static boolean isSpecialCharacter(final char c) {
+    return (!isUpperCaseLetter(c) && !isLowerCaseLetter(c) && !isDigit(c));
   }
 }
