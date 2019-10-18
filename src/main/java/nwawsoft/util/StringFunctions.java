87a0e820eeb925c19af22e73c49f37f07031a344
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2019-10-18 18:52:38 +0200

    Added more functions for Strings

--- src/main/java/nwawsoft/util/StringFunctions.java
+++ src/main/java/nwawsoft/util/StringFunctions.java
@@ -23,7 +23,7 @@ public class StringFunctions {
      * @param string the String to convert
      * @return the newly cased String.
      */
-    public static String toOnlyFirstCharCapital(String string) {
+    public static String toOnlyFirstCharCapital(final String string) {
         String outputString = string;
         if (outputString != null && !outputString.equals("")) {
             outputString = string.toLowerCase();
@@ -46,7 +46,7 @@ public class StringFunctions {
      * @return true if the first character is upper case or not a letter and every other character
      * is lower case or not a letter. Else false.
      */
-    public static boolean isOnlyFirstCharCapital(String string) {
+    public static boolean isOnlyFirstCharCapital(final String string) {
         if (string.equals(toOnlyFirstCharCapital(string))) {
             DebugPrinter.dp("Result of isOnlyFirstCharCapital(\"" + string + "\"): true");
             return true;
@@ -61,7 +61,7 @@ public class StringFunctions {
      * @param string the String to check
      * @return true if at least one letter was found. Else false.
      */
-    public static boolean containsLetter(String string) {
+    public static boolean containsLetter(final String string) {
         if (string != null) {
             for (int i = 0; i < string.length(); i++) {
                 if ((string.charAt(i) >= SMALL_A && string.charAt(i) <= SMALL_Z)
@@ -79,7 +79,7 @@ public class StringFunctions {
      * @param string the String to check
      * @return true if no other Character than smaller case a to z is found
      */
-    public static boolean isOnlyLowerCaseLetters(String string) {
+    public static boolean isOnlyLowerCaseLetters(final String string) {
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) >= SMALL_A && string.charAt(i) <= SMALL_Z) {
                 DebugPrinter.dp("Result of isOnlyLowerCaseLetters(\"" + string + "\"): " +
@@ -97,7 +97,7 @@ public class StringFunctions {
      * @param string the String to check
      * @return true if no other Character than upper case a to z is found
      */
-    public static boolean isOnlyUpperCaseLetters(String string) {
+    public static boolean isOnlyUpperCaseLetters(final String string) {
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) >= BIG_A && string.charAt(i) <= BIG_Z) {
                 DebugPrinter.dp("Result of isOnlyUpperCaseLetters(\"" + string + "\"): " +
@@ -114,7 +114,7 @@ public class StringFunctions {
      * @param pInput
      * @return
      */
-    public boolean isOnlyUpperCase(String pInput) {
+    public boolean isOnlyUpperCase(final String pInput) {
         char[] inputChars = pInput.toCharArray();
         for (int i = 0; i < inputChars.length; i++) {
             if (!Character.isUpperCase(inputChars[i])) {
@@ -130,7 +130,7 @@ public class StringFunctions {
      * @param string the String to check.
      * @return true if only allowed Characters (ASCII 0x61 to 0x7A and 0x5F) appear in the String.
      */
-    public static boolean isNormalized(String string) {
+    public static boolean isNormalized(final String string) {
         for (int i = 0; i < string.length(); i++) {
             char currentChar = string.charAt(i);
             if (!((currentChar >= SMALL_A && currentChar <= SMALL_Z)
@@ -149,7 +149,7 @@ public class StringFunctions {
      * @param string the String to check
      * @return true if only allowed Characters appear in the String
      */
-    public static boolean isFileName(String string) {
+    public static boolean isFileName(final String string) {
         for (int i = 0; i < string.length(); i++) {
             char currentChar = string.charAt(i);
             if (!(((currentChar >= BIG_A && currentChar <= BIG_Z)
@@ -172,7 +172,7 @@ public class StringFunctions {
      * @param string the String to check
      * @return true if only allowed Characters appear in the String
      */
-    public static boolean isFilePath(String string) {
+    public static boolean isFilePath(final String string) {
         for (int i = 0; i < string.length(); i++) {
             char currentChar = string.charAt(i);
             if (!(((currentChar >= BIG_A && currentChar <= BIG_Z)
@@ -196,7 +196,7 @@ public class StringFunctions {
      * @param string the String to check
      * @return true if only allowed Characters appear in the String
      */
-    public static boolean isFileIdentifier(String string) {
+    public static boolean isFileIdentifier(final String string) {
         for (int i = 0; i < string.length(); i++) {
             char currentChar = string.charAt(i);
             if (!(((currentChar >= BIG_A && currentChar <= BIG_Z)
@@ -224,7 +224,7 @@ public class StringFunctions {
      * @return true if only the specified character is included. Else false. Also returns false
      * when string is null.
      */
-    public static boolean containsOnly(String string, char match) {
+    public static boolean containsOnly(final String string, final char match) {
         if (string != null) {
             for (int i = 0; i < string.length(); i++) {
                 if (!(string.charAt(i) == match)) {
@@ -248,7 +248,7 @@ public class StringFunctions {
      * @param string some String
      * @return the new String without
      */
-    public static String removeWhiteSpaces(String string) {
+    public static String removeWhiteSpaces(final String string) {
         String newString = string.replaceAll("\\s", "");
         DebugPrinter.dp("Result of removeWhiteSpaces(\"" + string + "\"): \"" + newString
                 + "\"");
@@ -263,7 +263,7 @@ public class StringFunctions {
      * @param secondString another or equal String
      * @return true if Strings are similar, else false.
      */
-    public static boolean equalsStandardized(String firstString, String secondString) {
+    public static boolean equalsStandardized(final String firstString, final String secondString) {
         boolean output = removeWhiteSpaces(firstString).toLowerCase().equals(removeWhiteSpaces
                 (secondString).toLowerCase());
         DebugPrinter.dp("Result of equalsStandardized(\"" + firstString + "\", \""
@@ -271,7 +271,7 @@ public class StringFunctions {
         return output;
     }
 
-    public static String cutPathProtocol(String string) {
+    public static String cutPathProtocol(final String string) {
         String outputString;
         if (string.startsWith(FILE_PROTOCOL_MARKER)) {
             outputString = string.substring(FILE_PROTOCOL_MARKER.length());
@@ -283,5 +283,70 @@ public class StringFunctions {
                     + "\"");
             return string;
         }
+    }
+
+    /**
+     * Checks whether there is at least one lower case character somewhere in the specified String.
+     *
+     * @param input the String to check
+     * @return true if a lower case character was found. Else false.
+     */
+    public boolean containsLowerCaseCharacters(final String input) {
+        char[] inputArray = input.toCharArray();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (CharFunctions.isLowerCaseLetter(inputArray[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks whether there is at least one upper case character somewhere in the specified String.
+     *
+     * @param input the String to check
+     * @return true if an upper case character was found. Else false.
+     */
+    public boolean containsUpperCaseCharacters(final String input) {
+        char[] inputArray = input.toCharArray();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (CharFunctions.isUpperCaseLetter(inputArray[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks whether there is at least one special character somewhere in the specified String.
+     * A special character is every non-English-standard-letter (A-Z, a-z) or digit (0-9).
+     *
+     * @param input the String to check
+     * @return true if a special character was found. Else false.
+     */
+    public boolean containsSpecialCharacters(final String input) {
+        char[] inputArray = input.toCharArray();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (CharFunctions.isSpecialCharacter(inputArray[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks whether there is at least one digit somewhere in the specified String.
+     *
+     * @param input the String to check
+     * @return true if a digit was found. Else false.
+     */
+    public boolean containsDigits(final String input) {
+        char[] inputArray = input.toCharArray();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (CharFunctions.isDigit(inputArray[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
