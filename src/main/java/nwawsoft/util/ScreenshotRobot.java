4beee7a8340077ee7a42132b984f9d21494007f7
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2020-04-03 23:08:17 +0200

    Optimized ScreenshotRobot.

--- src/main/java/nwawsoft/util/ScreenshotRobot.java
+++ src/main/java/nwawsoft/util/ScreenshotRobot.java
@@ -10,45 +10,75 @@ import java.io.IOException;
  * Screenshots all monitors or a specified monitor.
  */
 public class ScreenshotRobot {
-	private Rectangle area;
+    private GraphicsDevice[] monitors;
+    private Rectangle area;
     private BufferedImage bi;
 
     /**
-     * Initializes a ScreenshotRobot and sets the dimensions of Rectangle "area" and the content of the 
-	 * BufferedImage "bi".
-     *
-     * @param screen 0 for all screens, else monitor number.
+     * Initializes a ScreenshotRobot.
      */
-    public ScreenshotRobot(final int screen) {
-		// ToDo: Split. Definition of area stays in constructor. Creation of BufferedImage goes into its own method.
-        GraphicsDevice[] monitors = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+    public ScreenshotRobot() {
+        init();
+    }
+
+    public static void main(String[] args) {
+        ScreenshotRobot sr = new ScreenshotRobot();
+        sr.configureArea(1);
+        sr.createBufferedImage();
+        sr.capture();
+    }
+
+    /**
+     * (Re-)Initializes the ScreenshotRobot.
+     */
+    public void init() {
+        monitors = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
         area = new Rectangle(0, 0, 0, 0);
         bi = null;
+    }
+
+    /**
+     * Configures the area of Rectangle "area", stores the content of the specified area into the BufferedImage "bi"
+     * and saves it as file "project_root/data/screenshot.png".
+     *
+     * @param screen 0 for all monitors, else monitor number starting with 1 for main monitor.
+     */
+    public void fullCapture(final int screen) {
+        configureArea(screen);
+        createBufferedImage();
+        capture();
+    }
+
+    /**
+     * Sets the Rectangle "area" depending on which monitor is specified.
+     *
+     * @param screen 0 for all monitors, else monitor number starting with 1 for main monitor.
+     */
+    public void configureArea(final int screen) {
         if (screen == 0) { // case: all monitors
             for (GraphicsDevice monitor : monitors) {
                 area = area.union(monitor.getDefaultConfiguration().getBounds());
             }
-            try {
-                bi = new Robot().createScreenCapture(area);
-            } catch (AWTException e) {
-                e.printStackTrace();
-            }
-        }
-        else if (screen < 0) { // case: error: negative screen number
+        } else if (screen < 0) { // case: error: negative screen number
             System.err.println("Negative monitor index specified. Doing nothing.");
-        }
-        else { // case monitor specified
-            try {
-                if (screen - 1 < monitors.length) {
-                    area = monitors[screen - 1].getDefaultConfiguration().getBounds();
-                    bi = new Robot().createScreenCapture(area);
-                } else {
-                    System.err.println("Not enough monitors found. Selected monitor " + screen + ". Found " + 
-						monitors.length + " monitors. Doing nothing.");
-                }
-            } catch (AWTException e) {
-                e.printStackTrace();
+        } else { // case: monitor specified
+            if (screen - 1 < monitors.length) {
+                area = monitors[screen - 1].getDefaultConfiguration().getBounds();
+            } else {
+                System.err.println("Not enough monitors found. Selected monitor " + screen + ". Found " +
+                        monitors.length + " monitors. Doing nothing.");
             }
+        }
+    }
+
+    /**
+     * Creates a BufferedImage of the Rectangle "area" and stores it into "bi".
+     */
+    public void createBufferedImage() {
+        try {
+            bi = new Robot().createScreenCapture(area);
+        } catch (AWTException e) {
+            e.printStackTrace();
         }
     }
 
@@ -65,10 +95,5 @@ public class ScreenshotRobot {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        ScreenshotRobot sr = new ScreenshotRobot(2);
-        sr.capture();
     }
 }
