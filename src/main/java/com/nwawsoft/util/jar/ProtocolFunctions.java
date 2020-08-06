85be1d3b576b5dea79cb611681431420e647dfd5
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2020-08-06 22:02:44 +0200

    Added class ProtocolFunctions which handles run environment detection.

---
+++ src/main/java/com/nwawsoft/util/jar/ProtocolFunctions.java
@@ -0,0 +1,18 @@
+package com.nwawsoft.util.jar;
+
+/**
+ * Provides functions which behave differently depending on the run environment (mainly IDE/direct execution vs. from
+ * inside a '.jar' file).
+ */
+public class ProtocolFunctions {
+
+    /**
+     * Returns whether a class has been loaded from a '.jar'.
+     *
+     * @param o the context. To check if a class itself runs from within a '.jar' use 'this'.
+     * @return true if o was loaded from a '.jar' file. Else false.
+     */
+    public boolean isInJar(final Object o) {
+        return o.getClass().getResource("").getProtocol().equals("jar");
+    }
+}
