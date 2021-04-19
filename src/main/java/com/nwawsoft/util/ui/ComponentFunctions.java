e23f033e61ce2df009a9ad7be3ae615801d12eb4
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2021-04-20 00:46:45 +0200

    Rewrote JavaDoc

--- src/main/java/com/nwawsoft/util/ui/ComponentFunctions.java
+++ src/main/java/com/nwawsoft/util/ui/ComponentFunctions.java
@@ -5,25 +5,25 @@ import com.nwawsoft.util.tools.DebugPrinter;
 import java.awt.*;
 
 /**
- * Supplies functions for awt {@code Component}s.
+ * Supplies functions for {@code java.awt.Component}s.
  */
 public class ComponentFunctions {
-
-    /**
-     * Centers a {@code Component} to the center of the screen.
-     *
-     * @param c any object of type {@code Component}.
-     */
-    public static void center(final Component c) {
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (d.width - c.getSize().width) / 2;
-        DebugPrinter.dp("d.width: " + d.width +
-                ", c.getSize().width: " +
-                c.getSize().width + ", x: " + x);
-        int y = (d.height - c.getSize().height) / 2;
-        DebugPrinter.dp("d.height: " + d.height +
-                ", c.getSize().height: " +
-                c.getSize().height + ", y: " + y);
-        c.setLocation(x, y);
-    }
+  
+  /**
+   * Centers a {@code java.awt.Component} to the center of the screen.
+   *
+   * @param c any object of type Component.
+   */
+  public static void center(final Component c) {
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - c.getSize().width) / 2;
+    DebugPrinter.dp("d.width: " + d.width +
+        ", c.getSize().width: " +
+        c.getSize().width + ", x: " + x);
+    int y = (d.height - c.getSize().height) / 2;
+    DebugPrinter.dp("d.height: " + d.height +
+        ", c.getSize().height: " +
+        c.getSize().height + ", y: " + y);
+    c.setLocation(x, y);
+  }
 }
